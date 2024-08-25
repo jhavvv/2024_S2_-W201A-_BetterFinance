6bf167f63f15b6b94ca5d0ab5da1d2e2c4163ad3
HazzaGitHubisshit <harrymg1208@gmail.com>
2024-08-26 10:35:39 +1200

    morning work
    yep

--- Pages/information.js
+++ Pages/information.js
@@ -1,33 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+// <!DOCTYPE html>
+// <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Information</title>
-</head>
-
-<body>
-    <h1>Information</h1>
-
-    <td>1, 2</td>
-
-    <p>Spending
-        <button>Add expense</button>
-
-    </p>
-
-    <p>Income
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Information</title>
+// </head>
 
 
-        <button>Add Income source</button>
-    </p>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './pagestyling.css';
 
-    <p>Regular Payees
+function App() {
+    return (
+
+        <body>
+            <h1>Information</h1>
+
+            <td>1, 2</td>
+
+            <p>Spending
+                <button>Add expense</button>
+
+            </p>
+
+            <p>Income
 
 
-        <button>Add Payee</button>
-    </p>
-</body>
+                <button>Add Income source</button>
+            </p>
 
-</html>
+            <p>Regular Payees
+
+
+                <button>Add Payee</button>
+            </p>
+        </body>
+    );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
