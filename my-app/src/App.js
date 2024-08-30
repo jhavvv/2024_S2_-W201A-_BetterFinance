f35fe40ca057dd60e47aba12e2ecda985d4e1171
HazzaGitHubisshit <harrymg1208@gmail.com>
2024-08-30 22:43:09 +1200

    informartion.js
    made it look better, also got it running with main myapp npm startt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Infopage from './Pages/information.js';
 import LoginPage from './LoginPage.js';
 import RegisterPage from './RegisterPage.js';
+
 
 function App() {
   return (
     <Router>
        <div className = "App">
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<Infopage />} />
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
         </Routes>
       </div>
     </Router>
