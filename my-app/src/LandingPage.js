de4d0c8e996363617800e4e129563fef12b9e04d
jhavvv <javeriaalinz@gmail.com>
2024-09-20 10:43:52 +1200

    made center

--- my-app/src/LandingPage.js
+++ my-app/src/LandingPage.js
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 import './LandingPage.css';
 import landingBackground from './assets/LandingBackground.jpg';
 
-function LandingPage (){
+function LandingPage() {
     const navigate = useNavigate();
 
     return (
@@ -15,8 +15,8 @@ function LandingPage (){
             <main>
                 <div className='title-cont' style={{ backgroundImage: `url(${landingBackground})` }}>
 
-                        <h1>BetterFinance</h1>
-                        <h2>Future Ready Finance</h2>
+                    <h1>BetterFinance</h1>
+                    <h2>Future Ready Finance</h2>
 
                     <div className='title-button-cont'>
                         <button className='title-button' onClick={() => navigate("/LoginPage")}>Login</button>
@@ -25,7 +25,9 @@ function LandingPage (){
                 </div>
 
                 <div className='benefits-cont'>
-                    <h2>BetterFinance Benefits</h2>
+                    <center>
+                        <td>
+                            <h2>BetterFinance Benefits</h2></td></center>
                     {/* Write about our product*/}
                     <p></p>
                 </div>
@@ -41,33 +43,33 @@ function LandingPage (){
                     {/* Write Facts*/}
                     <h3 className='fact-title'>Track Every Expense</h3>
                     <p className='fact-blurb'>Use an app or a spreadsheet to log every sinlge purchase. This helps you stay mindful of where your money is going and prevents small expenses from piling up unexpectedly</p>
-                    
+
                     <h3 className='fact-title'>Prioritze Needs Over Wants</h3>
                     <p className='fact-blurb'>When creating your budget, always focus on necessities first - things like rent, groceries, and utilities.
                         Once you've accounted for those, allocate money to savings, and finally, set aside a small portion for non-essential expenses.
                     </p>
-                   
+
                     <h3 className='fact-title'>Follow a Budgeting Rule</h3>
                     <p className='fact-blurb'>Following a simple rule of thumb like the 50/30/20 rule that suggests dividing your income into three categories:
                         50% for needs, 30% for wants, and 20% for savings or paying off debt.
                         It's a straightforward way to maintain financial balance without being overly restrictive.
                     </p>
                 </div>
-               
+
                 <div className='about-cont'>
                     <h2>Meet The Team</h2>
                     {/* Write About us*/}
                     <p></p>
                     <div className='dev-card-row'>
                         <div className='dev-card'>
-                            <img src="/HarryProfilePic.jpg" alt='Product Owner Harry' className='profile-img'/>
+                            <img src="/HarryProfilePic.jpg" alt='Product Owner Harry' className='profile-img' />
                             <h3>Harry</h3>
                             <p className='dev-title'>Product Owner</p>
-                            <p className='dev-about'>Harry is currently 19 years old, and is studying computer science at AUT. 
+                            <p className='dev-about'>Harry is currently 19 years old, and is studying computer science at AUT.
                                 In his spare time he enjoys listening to death metal and playing The Witcher.</p>
                         </div>
                         <div className='dev-card'>
-                            <img src="/JaveriaProfilePic.jpg" alt='Scrum Master Javeria' className='profile-img'/>
+                            <img src="/JaveriaProfilePic.jpg" alt='Scrum Master Javeria' className='profile-img' />
                             <h3>Javeria</h3>
                             <p className='dev-title'>Scrum Master</p>
                             <p className='dev-about'>Javeria is a very passionate young lady who strives on her creative freedom.
@@ -77,7 +79,7 @@ function LandingPage (){
                     </div>
                     <div className='dev-card-row'>
                         <div className='dev-card'>
-                            <img src="/MarcosProfilePic.jpg" alt='Developer Marcos' className='profile-img'/>
+                            <img src="/MarcosProfilePic.jpg" alt='Developer Marcos' className='profile-img' />
                             <h3>Marcos</h3>
                             <p className='dev-title'>Developer</p>
                             <p className='dev-about'>Marcos is 21 years old and he is studying Computer and Information Sciences at AUT.
@@ -85,24 +87,24 @@ function LandingPage (){
                             </p>
                         </div>
                         <div className='dev-card'>
-                            <img src="/NathanaelProfilePic.png" alt='Developer Nathanael' className='profile-img'/>
+                            <img src="/NathanaelProfilePic.png" alt='Developer Nathanael' className='profile-img' />
                             <h3>Nathanael</h3>
                             <p className='dev-title'>Developer</p>
-                            <p className='dev-about'>Nathanael is a 23 year old computer science student at AUT. 
+                            <p className='dev-about'>Nathanael is a 23 year old computer science student at AUT.
                                 In his free time he enjoys playing videos games and watching anime.</p>
                         </div>
                         <div className='dev-card'>
-                            <img src="/profile.jpg" alt='Developer Ijaz' className='profile-img'/>
+                            <img src="/profile.jpg" alt='Developer Ijaz' className='profile-img' />
                             <h3>Ijaz</h3>
                             <p className='dev-title'>Developer</p>
-                            <p className='dev-about'>Ijaz is 22 years old and studies Computer and Information Sciences at AUT. 
+                            <p className='dev-about'>Ijaz is 22 years old and studies Computer and Information Sciences at AUT.
                                 He likes to read and play games in his spare time.</p>
                         </div>
                     </div>
                     {/* Add Image related to us */}
                 </div>
-            </main>
-        </div>
+            </main >
+        </div >
     )
 }
 
