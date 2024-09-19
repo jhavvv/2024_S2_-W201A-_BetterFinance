2fc2a1da3fb35aadc8b424ebe13c3c44ec2c4c19
rjz1924 <163240520+rjz1924@users.noreply.github.com>
2024-09-20 10:32:52 +1200

    Nathanael (#50)
    * Create index.html
    
    * Added firebase and node modules
    
    :)
    
    * Created Collapsible Accordion-Style Drop-Down Text Boxes
    
    :)
    
    * Update App.js
    
    * Completed getting transactions from firestore
    
    Completed getting data from firestore and displaying the transactions
    
    * Added firebase connections for adding spending
    
    * Updated writing to firebase rules

--- my-app/src/information.js
+++ my-app/src/information.js
@@ -1,28 +1,13 @@
-// <!DOCTYPE html>
-// <html lang="en">
-
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Information</title>
-// </head>
-
-
-// import React from 'react';
 import React, { useState, useEffect } from 'react';
-
 import './Informationstyling.css';
-import './index.js'
-import './stylesheet.css'
+import './index.js';
+import './stylesheet.css';
 import Navbar from './Navbar';
 import { useNavigate } from 'react-router-dom';
-import { auth } from './firebase';
-
-
-
+import { auth, db } from './firebase';
+import { doc, collection, addDoc } from 'firebase/firestore';
 
 function Infopage() {
-
     const [income, setIncome] = useState('');
     const [incomeAmount, setIncomeAmount] = useState('');
     const [incomeFrequency, setIncomeFrequency] = useState('');
@@ -30,98 +15,133 @@ function Infopage() {
     const [spending, setSpending] = useState('');
     const [spendingAmount, setSpendingAmount] = useState('');
     const [spendingFrequency, setSpendingFrequency] = useState('');
-    const [Essentiality, setEssentiality] = useState('');
+    const [essentiality, setEssentiality] = useState('');
     const [category, setCategory] = useState('');
+    
     const [spendingDate, setSpendingDate] = useState('');
     const [spendingTime, setSpendingTime] = useState('');
 
-    
-        
     const [userName, setUserName] = useState('');
     const [userID, setUserID] = useState('');
     const navigate = useNavigate();
-    console.log("Current User:", auth.currentUser);
-
 
     useEffect(() => {
         const currentUser = auth.currentUser;
-
-        // If the user is logged in, extract their display name or email
         if (currentUser) {
             setUserName(currentUser.displayName || currentUser.email);
             setUserID(currentUser.uid);
         }
     }, []);
 
+    const handleAddIncome = async () => {
+        if (userID) {
+            try {
+                // Check if the input fields are not empty before proceeding
+                if (!income || !incomeAmount || !incomeFrequency) {
+                    console.error('All fields are required.');
+                    return;
+                }
 
+                const userDocRef = doc(db, 'users', userID);
+                const incomeCollectionRef = collection(userDocRef, 'income');
+                await addDoc(incomeCollectionRef, {
+                    incomeSource: income,
+                    amount: parseFloat(incomeAmount),
+                    frequency: incomeFrequency,
+                    timestamp: new Date()
+                });
+
+                console.log('Income added successfully');
+                // Navigate to SuccessPage with messageType state
+                navigate('/success', { state: { messageType: 'Income' } });
+            } catch (error) {
+                console.error('Error adding income:', error);
+            }
+        } else {
+            console.error('User not authenticated');
+        }
+    };
+
+    const handleAddSpending = async () => {
+        if (userID) {
+            try {
+                // Check if the input fields are not empty before proceeding
+                if (!spending || !spendingAmount || !spendingFrequency || !essentiality || !category || !spendingDate || !spendingTime) {
+                    console.error('All fields are required.');
+                    return;
+                }
+
+                const userDocRef = doc(db, 'users', userID);
+                const spendingCollectionRef = collection(userDocRef, 'spending');
+                await addDoc(spendingCollectionRef, {
+                    spendingSource: spending,
+                    amount: parseFloat(spendingAmount),
+                    frequency: spendingFrequency,
+                    essentiality: essentiality,
+                    category: category,
+                    date: spendingDate,
+                    time: spendingTime,
+                    timestamp: new Date()
+                });
+
+                console.log('Spending added successfully');
+                // Navigate to SuccessPage with messageType state
+                navigate('/success', { state: { messageType: 'Spending' } });
+            } catch (error) {
+                console.error('Error adding spending:', error);
+            }
+        } else {
+            console.error('User not authenticated');
+        }
+    };
 
     return (
-
         <div>
             <header>
-
                 <Navbar />
             </header>
-
-            
-
-
 
             <div id="info-container">
                 <h1>Information</h1>
 
                 <div id="boxes-container">
+                    {/* Income Section */}
                     <div className="info-box">
                         <p>Income</p>
-                        <label> Add income: </label>
+                        <label htmlFor="income">Add income:</label>
                         <input
+                            id="income"
                             type="text"
                             value={income}
                             onChange={(e) => setIncome(e.target.value)}
                             required
                         />
-                        <label> Amount: </label>
+                        <label>Amount:</label>
                         <input
                             type="number"
+                            min="0"
                             value={incomeAmount}
                             onChange={(e) => setIncomeAmount(e.target.value)}
                             required
                         />
-                        <label>What Date and time was this:</label>
-                        <label>Date: </label>
-                        <input
-                            type="date"
-                            value={spendingDate}
-                            onChange={(e) => setSpendingDate(e.target.value)}
-                            required
-                        />
-                        <label>Time:</label>
-                        <input
-                            type="time"
-                            value={spendingTime}
-                            onChange={(e) => setSpendingTime(e.target.value)}
-                            required
-                        />
-                        <label> How often: </label>
+                        <label>How often:</label>
                         <select
                             value={incomeFrequency}
                             onChange={(e) => setIncomeFrequency(e.target.value)}
                             required
                         >
-                            <option value="">Select frequency(N/A if one-off)</option>
+                            <option value="">Select frequency (N/A if one-off)</option>
                             <option value="daily">Daily</option>
                             <option value="weekly">Weekly</option>
                             <option value="monthly">Monthly</option>
                         </select>
-
-                        <button type="submit"> Add Income source </button>
+                        <button type="button" onClick={handleAddIncome}>Add Income source</button>
                     </div>
 
-
-
+                    {/* Spending Section */}
                     <div className="info-box">
                         <p>Spending</p>
-                        <label> Add expense: </label>
+                        <label>Add expense:</label>
                         <input
                             type="text"
                             value={spending}
@@ -132,14 +152,13 @@ function Infopage() {
                         <p className="essential-label">Was this expense essential or non-essential?</p>
 
                         <div className="radio-container">
-
                             <div className="radio-item">
                                 <input
                                     type="radio"
                                     id="essential"
                                     name="purchase"
                                     value="Essential"
-                                    checked={Essentiality === 'Essential'}
+                                    checked={essentiality === 'Essential'}
                                     onChange={(e) => setEssentiality(e.target.value)}
                                 />
                                 <label htmlFor="essential">Essential</label>
@@ -151,14 +170,14 @@ function Infopage() {
                                     id="non-essential"
                                     name="purchase"
                                     value="Non-Essential"
-                                    checked={Essentiality === 'Non-Essential'}
+                                    checked={essentiality === 'Non-Essential'}
                                     onChange={(e) => setEssentiality(e.target.value)}
                                 />
                                 <label htmlFor="non-essential">Non-Essential</label>
                             </div>
                         </div>
 
-                        <label>Category: </label>
+                        <label>Category:</label>
                         <select
                             value={category}
                             onChange={(e) => setCategory(e.target.value)}
@@ -172,23 +191,23 @@ function Infopage() {
                             <option value="others">Others</option>
                         </select>
 
-
-                        <label>Amount($): </label>
+                        <label>Amount($):</label>
                         <input
                             type="number"
+                            min="0"
                             value={spendingAmount}
                             onChange={(e) => setSpendingAmount(e.target.value)}
                             required
                         />
 
-                        <label>What Date and time was this:</label>
-                        <label>Date: </label>
+                        <label>Date:</label>
                         <input
                             type="date"
                             value={spendingDate}
                             onChange={(e) => setSpendingDate(e.target.value)}
                             required
                         />
+
                         <label>Time:</label>
                         <input
                             type="time"
@@ -197,27 +216,24 @@ function Infopage() {
                             required
                         />
 
-                        <label> How often: </label>
+                        <label>How often:</label>
                         <select
                             value={spendingFrequency}
                             onChange={(e) => setSpendingFrequency(e.target.value)}
                             required
                         >
-                            <option value="">Select frequency(N/A if one-off)</option>
+                            <option value="">Select frequency </option>
+                            <option value="one-off">One-Off</option>
                             <option value="daily">Daily</option>
                             <option value="weekly">Weekly</option>
                             <option value="monthly">Monthly</option>
                         </select>
-                        <button type="submit"> Add Expense </button>
+                        <button type="button" onClick={handleAddSpending}>Add Expense</button>
                     </div>
-
                 </div>
             </div>
         </div>
-
-
-
     );
 }
-// ReactDOM.render(<App />, document.getElementById('root'));
+
 export default Infopage;
